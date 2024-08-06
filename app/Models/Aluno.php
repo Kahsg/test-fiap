28a96973abd56415d759dcad48041e3007fcc30f
kathia.gimenes <kathia.gimenes@gmail.com>
2024-08-06 08:34:59 -0300

    Cria tabela de alunos

---
+++ app/Models/Aluno.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Aluno extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $connection = 'mysql';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'uuid',
+        'nome',
+        'nascimento',
+        'usuario',
+    ];
+
+    public $casts = [
+        'nascimento' => 'datetime:Y-m-d',
+    ];
+}
