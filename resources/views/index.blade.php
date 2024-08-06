b6c4839f25aade509e689b5b1566d567130648a0
kathia.gimenes <kathia.gimenes@gmail.com>
2024-08-06 08:34:46 -0300

    Cria página home.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master-layout')
+@section('title')
+    Home
+@endsection
+
+@section('content')
+<div class="row text-center m-5">
+    <h3>AVALIAÇÃO DE PHP - FIAP</h3>
+    <h5 class="text-muted">Kathia Schaffer Gimenes</h5>
+</div>
+@endsection
