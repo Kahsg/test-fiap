8bfd1ab1343cfaf8872793ee9906a41082429630
kathia.gimenes <kathia.gimenes@gmail.com>
2024-08-06 08:16:51 -0300

    Cria layout básico

---
+++ resources/views/layouts/topnav.blade.php
@@ -0,0 +1,6 @@
+<div class="topnav">
+    <a class="active" href="{{route('home')}}">Home</a>
+    <a href="{{route('home')}}">Alunos</a>
+    <a href="{{route('home')}}">Turmas</a>
+    <a href="{{route('home')}}">Matrículas</a>
+</div>
