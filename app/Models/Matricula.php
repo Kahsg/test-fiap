715484d2ba2dbc862d3b33c387252812e94227ce
kathia.gimenes <kathia.gimenes@gmail.com>
2024-08-07 16:50:23 -0300

    Cria tabela de matrículas.

---
+++ app/Models/Matricula.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Matricula extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $connection = 'mysql';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'uuid',
+        'turma_id',
+        'aluno_id',
+    ];
+
+    public function turma()
+    {
+        return $this->hasOne(Turma::class, 'id', 'turma_id');
+    }
+
+    public function aluno()
+    {
+        return $this->hasOne(Turma::class, 'id', 'aluno_id');
+    }
+}
---
+++ database/migrations/2024_08_07_000002_create_matriculas_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('matriculas', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')
+                ->unique();
+            $table->unsignedBigInteger('turma_id')
+                ->comment('Id da turma')
+                ->index('turma_id_foreign');
+            $table->foreign('turma_id')
+                ->references('id')
+                ->on('turmas');
+            $table->unsignedBigInteger('aluno_id')
+                ->comment('Id do aluno')
+                ->index('aluno_id_foreign');
+            $table->foreign('aluno_id')
+                ->references('id')
+                ->on('turma_tipos');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('matriculas');
+    }
+};
