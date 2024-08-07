24a390343be3abe7b6be70f09a28b79e12279f1f
kathia.gimenes <kathia.gimenes@gmail.com>
2024-08-07 12:59:04 -0300

    Insere rota de turmas no menu.

--- resources/views/layouts/topnav.blade.php
+++ resources/views/layouts/topnav.blade.php
@@ -1,6 +1,6 @@
 <div class="topnav">
     <a class="active" href="{{route('home')}}">Home</a>
     <a href="{{route('alunos.index')}}">Alunos</a>
-    <a href="{{route('home')}}">Turmas</a>
+    <a href="{{route('turmas.index')}}">Turmas</a>
     <a href="{{route('home')}}">Matrículas</a>
 </div>
