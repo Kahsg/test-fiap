3a7366564bbfec547e8a422dcf8a04067c3f257b
kathia.gimenes <kathia.gimenes@gmail.com>
2024-08-08 12:55:22 -0300

    Atualiza checagem de menu ativo.

--- resources/views/layouts/topnav.blade.php
+++ resources/views/layouts/topnav.blade.php
@@ -3,9 +3,9 @@
 
         <div class="col">
             <a class="{{request()->routeIs('home') ? 'active' : ''}}" href="{{route('home')}}">Home</a>
-            <a class="{{request()->routeIs('alunos.index') ? 'active' : ''}}" href="{{route('alunos.index')}}">Alunos</a>
-            <a class="{{request()->routeIs('turmas.index') ? 'active' : ''}}" href="{{route('turmas.index')}}">Turmas</a>
-            <a class="{{request()->routeIs('matriculas.index') ? 'active' : ''}}" href="{{route('matriculas.index')}}">Matrículas</a>
+            <a class="{{request()->routeIs('alunos.*') ? 'active' : ''}}" href="{{route('alunos.index')}}">Alunos</a>
+            <a class="{{request()->routeIs('turmas.*') ? 'active' : ''}}" href="{{route('turmas.index')}}">Turmas</a>
+            <a class="{{request()->routeIs('matriculas.*') ? 'active' : ''}}" href="{{route('matriculas.index')}}">Matrículas</a>
         </div>
         <div class="col-1">
             <form method="POST" action="{{ route('logout') }}">
